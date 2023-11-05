64df2df10b54d96ae25f9ef0c281feb9599701e0
git-bruh <e817509a-8ee9-4332-b0ad-3a6bdf9ab63f@aleeas.com>
2023-11-05 18:01:13 +0530

    *: compile 16-bit asm at build time and embed it

--- src/main.rs
+++ src/main.rs
@@ -5,15 +5,7 @@ use std::os::fd::BorrowedFd;
 use vmm::kvm::Kvm;
 use vmm::util::WrappedAutoFree;
 
-const CODE: [u8; 12] = [
-    0xba, 0xf8, 0x03, /* mov $0x3f8, %dx */
-    0x00, 0xd8, /* add %bl, %al */
-    0x04, b'0', /* add $'0', %al */
-    0xee, /* out %al, (%dx) */
-    0xb0, b'\n', /* mov $'\n', %al */
-    0xee,  /* out %al, (%dx) */
-    0xf4,  /* hlt */
-];
+const CODE: &[u8] = include_bytes!(concat!(env!("OUT_DIR"), "/", "write_serial"));
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let kvm = Kvm::new()?;
