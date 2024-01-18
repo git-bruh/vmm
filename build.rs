050a0adbb546f79876dfe9b17226ac11edb4039e
git-bruh <prathamIN@proton.me>
2024-01-18 16:52:58 +0530

    build.rs: use header_contents instead of header

--- build.rs
+++ build.rs
@@ -3,7 +3,7 @@ use std::{env, path::PathBuf};
 fn main() {
     let bootparam = PathBuf::from(env::var("OUT_DIR").unwrap()).join("bootparam.rs");
     bindgen::builder()
-        .header("bindgen/bootparam.h")
+        .header_contents("bootparam.h", "#include <asm/bootparam.h>")
         .derive_default(true)
         .generate()
         .expect("unable to generate bindings for bootparam.h")
